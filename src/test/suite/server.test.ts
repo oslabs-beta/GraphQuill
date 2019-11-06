1205e2e61f8b4485d81553f0c6da6dfd66851591
alexchao26 <alexchaoo@gmail.com>
2019-11-05 21:03:55 -0500

    toggling feature added. ditched server testing for now because of importing in vscode library...

--- src/test/suite/server.test.ts
+++ src/test/suite/server.test.ts
@@ -1,7 +1,4 @@
-// ! ALL CODE IS COPIED FROM PARSER.TEST.JS, NEEDS TO BE UPDATED TO TEST ANY SERVER STUFF
-// ! Also might need super test...
-
-// ! I need to look into how to even test a server/terminal/child process...
+// ! I'm not sure how to add tests to this.
 
 // * THIS LINE SETS THE ENVIRONMENT TO MOCHA, so describe, it etc. are not undefined
 /* eslint-env mocha */
